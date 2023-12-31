26105c596b1b471950267c757744a2f90a385ec4
InvitacionDigital <alenmonti0@gmail.com>
2023-12-31 16:49:54 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,16 @@ const Hero: React.FC = () => {
   return (
     <div className='bg-[url("/bg6.jpeg")] opacity-80 w-full min-w-[300px] max-w-[1000px] aspect-[2/3] bg-contain bg-center bg-no-repeat flex flex-col justify-center py-10 items-center relative '>
       <div className='flex items-center gap-2'>
-        <span className='border-b border-secondary w-14 h-0'></span>
-        <p className='font-[poppins] text-secondary'>10.02.2024</p>
-        <span className='border-b border-secondary w-14 h-0'></span>
+        <span className='border-b border-primary w-14 h-0'></span>
+        <p className='font-[poppins] text-primary'>10.02.2024</p>
+        <span className='border-b border-primary w-14 h-0'></span>
       </div>
       <div className='text-7xl text-center w-[205px] relative my-12'>
         <h2 className='text-primary opacity-30 text-[10rem] absolute top-1/2 -translate-y-1/2'>XV</h2>
         <h1 className='z-30 relative font-semibold'>Mora</h1>
         <h1 className='text-xl relative '>Mis 15 años</h1>
       </div>
-      <span className='border-b border-secondary w-44 h-0'></span>
+      <span className='border-b border-primary w-44 h-0'></span>
 
       <GoDown />
     </div>
